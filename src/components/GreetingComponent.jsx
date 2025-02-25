271a2f37f3cc11190cf3ddd7a7c5a34625e0f9f7
Magali <maga.bjb@gmail.com>
2025-02-25 16:41:31 -0300

    TP finalizado

--- src/components/GreetingComponent.jsx
+++ src/components/GreetingComponent.jsx
@@ -3,7 +3,7 @@ import './GreetingComponent.css';
 export default function GreetingComponent({ greeting }) {
     return (
         <div className="hero-container">
-            <img className="imagenHero" src="../public/imagen hero.png" alt="imagen hero" />
+            <img className="imagenHero" src="/imagen hero.png" alt="imagen hero" />
             <h1 className="texto-encima">{greeting}</h1>
         </div>
     );
